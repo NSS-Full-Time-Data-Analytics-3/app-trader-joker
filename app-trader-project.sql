5ecd2be3f0b19f9052549b2c9929f0ee1757b0bc
BrandonM471998 <brandontmorgan3@gmail.com>
2021-02-14 18:00:03 -0600

    narrowed down apps to top 20

--- app-trader-project.sql
+++ app-trader-project.sql
@@ -1,15 +1,16 @@
---
-SELECT CAST(price AS money), rating, name, 
-	review_count, content_rating, genres
-		FROM play_store_apps
-		ORDER BY review_count DESC;
---
-SELECT CAST(price AS money), rating, name, 
-	CAST(review_count AS integer), content_rating, primary_genre
-		FROM app_store_apps
-		ORDER BY review_count DESC;
+
 --
 
+SELECT ios.name, ios.rating AS ios_rating, 
+		android.rating AS android_rating, CAST(ios.price AS money) AS ios_price,
+		 CAST(android.price AS money) AS android_price, CAST(ios.review_count AS integer) AS ios_review,
+	 	android.review_count AS android_review, ios.primary_genre AS ios_genre,
+		android.genres AS android_genre
+			FROM play_store_apps AS android FULL JOIN app_store_apps AS ios
+			ON ios.name=android.name
+				WHERE ios IS NOT NULL 
+				AND android IS NOT NULL
+					ORDER BY ios_rating DESC;
 --2 a)
 --2 b)
 --2 c)
--- data/app-trader-project.sql
+++ data/app-trader-project.sql
@@ -1,17 +1,22 @@
---
-SELECT CAST(price AS money), rating, name, review_count, content_rating
-FROM play_store_apps;
---
-SELECT CAST(price AS money), rating, name, CAST(review_count AS integer), content_rating
-	FROM app_store_apps
-	ORDER BY review_count DESC;
---
 
---2 a)
---2 b)
---2 c)
---2 d)
---2 e)
---3 a)
---3 b)
---3 c)
+--
+WITH both_store_apps AS (SELECT ios.name, ios.rating AS ios_rating, 
+						android.rating AS android_rating, CAST(ios.price AS money) AS ios_price,
+		 				CAST(android.price AS money) AS android_price, CAST(ios.review_count AS integer) AS ios_review,
+	 					android.review_count AS android_review, ios.primary_genre AS ios_genre,
+						android.genres AS android_genre
+						FROM play_store_apps AS android FULL JOIN app_store_apps AS ios
+						ON ios.name=android.name
+						WHERE ios IS NOT NULL 
+						AND android IS NOT NULL
+						ORDER BY ios_rating DESC),
+
+	 top_20 AS (SELECT *
+				FROM both_store_apps
+				WHERE android_rating >= (SELECT ROUND(AVG(android_rating)) FROM both_store_apps)
+				AND ios_rating >= (SELECT ROUND(AVG(ios_rating)) FROM both_store_apps)
+				ORDER BY android_price ASC
+				LIMIT 20)
+
+SELECT *
+FROM top_20;
